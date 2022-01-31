fbb9db7ac170433b920d596f898db76e229e1023
madson05 <96205881+Madson05@users.noreply.github.com>
2022-01-31 16:18:32 -0300

    Criação de novo projeto de um To-do List

---
+++ To-Do List/assets/script.js
@@ -0,0 +1,35 @@
+const ENTRADA = document.getElementById("myform");
+const MINHA_CHECK = document.getElementById("lista");
+
+
+function getTexto(){
+    let texto = document.getElementById("texto");
+    addBox(texto.value);
+    ENTRADA.reset();
+};
+
+function addBox(tarefa){
+    let novaDiv = document.createElement("div");
+    let novaTarefa = document.createElement("input");
+    let novaLabel = document.createElement("label");
+    let descricaoTarefa = document.createTextNode(tarefa);
+
+    novaTarefa.setAttribute("type", "checkbox");
+    novaTarefa.setAttribute("name", tarefa);
+    novaTarefa.setAttribute("id", tarefa);
+
+
+    novaLabel.setAttribute("for", tarefa);
+    novaLabel.appendChild(descricaoTarefa)
+
+
+    novaDiv.classList.add("task-item");
+    novaDiv.appendChild(novaTarefa);
+    novaDiv.appendChild(descricaoTarefa);
+
+
+    MINHA_CHECK.appendChild(novaDiv);
+
+
+    
+}
