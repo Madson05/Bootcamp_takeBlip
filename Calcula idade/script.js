c08162e0fdad66d992a9157361d61f8d35735beb
Madson Silva <madsonsilva2005@gmail.com>
2022-02-07 13:29:34 -0300

    Update script.js

--- Calcula idade/script.js
+++ Calcula idade/script.js
@@ -7,11 +7,15 @@ const pessoa2 = {
     nome: "Paulo",
     idade: 26,
 };
+const pessoa2 = {
+    nome: "José",
+    idade: 29,
+};
 
 
 
 function calculadorDeIdade(anos){
-    return `daqui a ${anos} anos ${this.nome} terá ${this.idade + anos} anos`
+    return `Daqui a ${anos} anos ${this.nome} terá ${this.idade + anos} anos`
 };
 
 console.log(calculadorDeIdade.call(pessoa1, 30));
