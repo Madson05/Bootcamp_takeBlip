4e83712ee6ac941350e3477d4be9a05538f52e58
Madson Silva <madsonsilva2005@gmail.com>
2022-03-13 17:43:38 -0300

    iniciado novo projeto prático (Jogo Resgate)

---
+++ Jogo Resgate/assets/js/script.js
@@ -0,0 +1,111 @@
+function start() { // Inicio da função start()
+
+	$("#inicio").hide();
+	
+	$("#fundoGame").append("<div id='jogador' class='anima1' > </div>");
+	$("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");
+	$("#fundoGame").append("<div id='inimigo2'></div>");
+	$("#fundoGame").append("<div id='amigo' ' class='anima2'></div>");
+
+	//Principais variáveis do jogo
+	
+	var jogo = {}
+
+	var velocidade=5;
+	var posicaoY = parseInt(Math.random() * 334);
+
+
+	var TECLA = {
+		W: 87,
+		S: 83,
+		D: 68
+	}
+
+
+	
+	jogo.pressionou = [];
+
+	//Verifica se o usuário pressionou alguma tecla	
+	
+	$(document).keydown(function(e){
+	jogo.pressionou[e.which] = true;
+	});
+	
+	
+	$(document).keyup(function(e){jogo.pressionou[e.which] = false;
+	});
+	
+	//Game Loop
+
+	jogo.timer = setInterval(loop,30);
+	
+	function loop() {
+	
+	movefundo();
+	movejogador();
+	moveinimigo1();
+
+	
+	} // Fim da função loop()
+
+	//Função que movimenta o fundo do jogo
+	
+	function movefundo() {
+	
+		esquerda = parseInt($("#fundoGame").css("background-position"));
+		$("#fundoGame").css("background-position",esquerda-1);
+		
+	} // fim da função movefundo()
+
+	function movejogador() {
+	
+		if (jogo.pressionou[TECLA.W]) {
+			var topo = parseInt($("#jogador").css("top"));
+			$("#jogador").css("top",topo-10);
+
+			if (topo<=0) {
+		
+				$("#jogador").css("top",topo+10);
+			}
+		
+		}
+		
+		if (jogo.pressionou[TECLA.S]) {
+			
+			var topo = parseInt($("#jogador").css("top"));
+			$("#jogador").css("top",topo+10);
+			
+			if (topo>=434) {	
+				$("#jogador").css("top",topo-10);
+			}
+		}
+		
+		if (jogo.pressionou[TECLA.D]) {
+			
+			//Chama função Disparo	
+		}
+	
+	} // fim da função movejogador()
+
+	function moveinimigo1() {
+
+		posicaoX = parseInt($("#inimigo1").css("left"));
+		$("#inimigo1").css("left",posicaoX-velocidade);
+		$("#inimigo1").css("top",posicaoY);
+			
+			if (posicaoX<=0) {
+			posicaoY = parseInt(Math.random() * 334);
+			$("#inimigo1").css("left",694);
+			$("#inimigo1").css("top",posicaoY);
+				
+			}
+	} //Fim da função moveinimigo1()
+	
+	
+
+
+
+
+} // Fim da função start
+
+
